de1aaabe1b63c18c025411276dd856c8e3dc5f68
GrooorV <gamhust@gmail.com>
2025-10-06 22:00:33 +0500

    Revert "task1"

--- src/main/java/newsbot/engine/UserSession.java
+++
@@ -1,9 +0,0 @@
-package newsbot.engine;
-
-public class UserSession {
-    private boolean started;
-
-    public boolean isStarted() { return started; }
-    public void markStarted() { started = true; }
-    public void unmarkStarted() { started = false; }
-}
--- src/main/java/newsbot/repository/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package newsbot.repository;
-
-import newsbot.engine.UserSession;
-import newsbot.shared.UserId;
-
-public interface SessionRepository {
-    UserSession getOrCreate(UserId userId);
-    void save(UserId userId, UserSession session);
-}
