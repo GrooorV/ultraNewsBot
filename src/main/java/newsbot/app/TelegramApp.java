e5d18884b145ec4931dd82bab5d574e1aa27937e
art <shidstree@gmail.com>
2025-11-24 23:19:04 +0500

    added TelegramApp.java

---
+++ src/main/java/newsbot/app/TelegramApp.java
@@ -0,0 +1,64 @@
+package newsbot.app;
+
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import newsbot.console.TelegramAdapter;
+import newsbot.engine.DialogueService;
+import newsbot.engine.DialogueEngine;
+import newsbot.network.DataFetcher;
+import newsbot.network.HttpDataFetcher;
+import newsbot.news.*;
+import newsbot.repository.NewsRepository;
+import newsbot.repository.SessionRepository;
+import newsbot.repository.UserProfileRepository;
+import newsbot.repository.memory.InMemoryNewsRepository;
+import newsbot.repository.memory.InMemorySessionRepository;
+import newsbot.repository.memory.InMemoryUserProfileRepository;
+
+// Импорты команд
+import newsbot.command.*;
+import newsbot.command.resolver.CommandResolver;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class TelegramApp {
+    public static void main(String[] args) throws TelegramApiException {
+        DataFetcher fetcher = new HttpDataFetcher();
+        LentaRssParser  parser = new LentaRssParser(fetcher);
+
+        NewsProvider newsProvider = new LentaNewsProvider(fetcher, parser);
+
+        UserProfileRepository userProfileRepo = new InMemoryUserProfileRepository();
+        SessionRepository sessionRepo = new InMemorySessionRepository();
+        NewsRepository newsRepo = new InMemoryNewsRepository(newsProvider);
+
+        NewsFeedGenerator feedGenerator = new NewsFeedGenerator(userProfileRepo, sessionRepo, newsRepo);
+
+        DialogueEngine engine = new DialogueEngine();
+        NewsPreferenceService newsPrefs = new NewsPreferenceService(userProfileRepo);
+
+        BotCommand helpCommand = new HelpCommand();
+        BotCommand availableCommand = new AvailableCommand(newsPrefs);
+        BotCommand newsCommand = new NewsCommand(feedGenerator);
+        BotCommand categoryCommand = new CategoryCommand(newsPrefs, sessionRepo);
+        BotCommand setCategoriesCommand = new SetCategoriesCommand(newsPrefs, sessionRepo);
+        BotCommand changeUserCommand = new ChangeProfileCommand();
+        BotCommand whoAmICommand = new WhoAmICommand();
+
+        CommandResolver commandResolver = new CommandResolver(setCategoriesCommand);
+
+        commandResolver.register(helpCommand);
+        commandResolver.register(availableCommand);
+        commandResolver.register(newsCommand);
+        commandResolver.register(categoryCommand);
+        commandResolver.register(changeUserCommand);
+        commandResolver.register(whoAmICommand);
+
+        DialogueService dialogueService = new DialogueService(engine, sessionRepo, commandResolver);
+
+        String botToken = "token"; //temp token var for testing
+
+        TelegramAdapter telegramAdapter = new TelegramAdapter(botToken, dialogueService);
+        TelegramBotsLongPollingApplication app = new TelegramBotsLongPollingApplication();
+        app.registerBot(botToken, telegramAdapter);
+    }
+}
