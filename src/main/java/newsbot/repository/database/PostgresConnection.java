18b724ea6b4a644fb616d6e81527cf9e96177323
art <shidstree@gmail.com>
2025-12-08 22:45:00 +0500

    realized PostgresConnection

--- src/main/java/newsbot/repository/database/PostgresConnection.java
+++ src/main/java/newsbot/repository/database/PostgresConnection.java
@@ -1,10 +1,16 @@
 package newsbot.repository.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class PostgresConnection {
 
-    private static Connection getConnection() {
-        return null;
+    public static Connection getConnection() throws SQLException {
+        String url = System.getenv("url");
+        String user = System.getenv("user");
+        String password = System.getenv("password");
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
