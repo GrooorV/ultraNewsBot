2421b2199986a49ac26cb58b6d189494c48cb7cf
art <shidstree@gmail.com>
2025-12-09 00:50:19 +0500

    realized DatabaseUserProfileRepository

--- src/main/java/newsbot/repository/database/DatabaseUserProfileRepository.java
+++ src/main/java/newsbot/repository/database/DatabaseUserProfileRepository.java
@@ -1,78 +1,78 @@
-package newsbot.repository.database;
+    package newsbot.repository.database;
 
-import newsbot.news.NewsCategory;
-import newsbot.repository.UserProfileRepository;
-import newsbot.shared.UserId;
+    import newsbot.news.NewsCategory;
+    import newsbot.repository.UserProfileRepository;
+    import newsbot.shared.UserId;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
+    import java.sql.Connection;
+    import java.sql.PreparedStatement;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
+    import java.util.HashSet;
+    import java.util.Set;
 
-public class DatabaseUserProfileRepository implements UserProfileRepository {
-    @Override
-    public boolean categoriesAreEmpty(UserId userId) {
-        Set<NewsCategory> set = getCategories(userId);
-        return set.isEmpty();
-    }
+    public class DatabaseUserProfileRepository implements UserProfileRepository {
+        @Override
+        public boolean categoriesAreEmpty(UserId userId) {
+            Set<NewsCategory> set = getCategories(userId);
+            return set.isEmpty();
+        }
 
-    @Override
-    public Set<NewsCategory> getCategories(UserId userId) {
-        String sql = "SELECT Category FROM user_categories WHERE UserId = ?";
+        @Override
+        public Set<NewsCategory> getCategories(UserId userId) {
+            String sql = "SELECT category FROM users_categories WHERE user_id = ?";
 
-        Set<NewsCategory> set = new HashSet<>();
+            Set<NewsCategory> set = new HashSet<>();
 
-        try (Connection conn = PostgresConnection.getConnection();
-             PreparedStatement statement = conn.prepareStatement(sql)) {
+            try (Connection conn = PostgresConnection.getConnection();
+                 PreparedStatement statement = conn.prepareStatement(sql)) {
 
-            statement.setString(1, userId.getValue());
+                statement.setString(1, userId.getValue());
 
-            try(ResultSet rs = statement.executeQuery()) {
+                try(ResultSet rs = statement.executeQuery()) {
 
-                while (rs.next()) {
-                    set.add(NewsCategory.valueOf(rs.getString("Category")));
+                    while (rs.next()) {
+                        set.add(NewsCategory.valueOf(rs.getString("category")));
+                    }
                 }
+
+            } catch (SQLException e) {
+                System.err.println("Error while get categories: " + e.getMessage());
             }
-
-        } catch (SQLException e) {
-            System.err.println("Error while get categories: " + e.getMessage());
+            return set;
         }
-        return set;
-    }
 
-    @Override
-    public void addCategory(UserId userId, NewsCategory newsCategory) {
-        String sql = "INSERT INTO users_categories (UserId, Category) values (?, ?)" +
-                "ON CONFLICT (UserId, Category) DO NOTHING";
+        @Override
+        public void addCategory(UserId userId, NewsCategory newsCategory) {
+            String sql = "INSERT INTO users_categories (user_id, category) values (?, ?)" +
+                    "ON CONFLICT (user_id, category) DO NOTHING";
 
-        try (Connection conn = PostgresConnection.getConnection();
-             PreparedStatement statement = conn.prepareStatement(sql)) {
+            try (Connection conn = PostgresConnection.getConnection();
+                 PreparedStatement statement = conn.prepareStatement(sql)) {
 
-            statement.setString(1, userId.getValue());
-            statement.setString(2, newsCategory.getName());
+                statement.setString(1, userId.getValue());
+                statement.setString(2, newsCategory.name());
 
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            System.err.println("Error while add category: " + e.getMessage());
+                statement.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("Error while add category: " + e.getMessage());
+            }
         }
-    }
 
-    @Override
-    public void removeCategory(UserId userId, NewsCategory newsCategory) {
-        String sql = "DELETE FROM users_categories WHERE UserId = ? AND Category = ?";
+        @Override
+        public void removeCategory(UserId userId, NewsCategory newsCategory) {
+            String sql = "DELETE FROM users_categories WHERE user_id = ? AND category = ?";
 
-        try(Connection conn = PostgresConnection.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql)) {
+            try(Connection conn = PostgresConnection.getConnection();
+                PreparedStatement statement = conn.prepareStatement(sql)) {
 
-            statement.setString(1, userId.getValue());
-            statement.setString(2, newsCategory.getName());
+                statement.setString(1, userId.getValue());
+                statement.setString(2, newsCategory.name());
 
-            statement.executeUpdate();
+                statement.executeUpdate();
 
-        } catch (SQLException e) {
-            System.err.println("Error while remove category: " + e.getMessage());
+            } catch (SQLException e) {
+                System.err.println("Error while remove category: " + e.getMessage());
+            }
         }
     }
-}
